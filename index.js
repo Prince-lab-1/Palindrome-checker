76dfbaeed304e916e3322e8df2c71163a340bde3
TurikumanaFiston <turikumanafiston6@gmail.com>
2025-01-25 06:21:35 +0200

    this is palindrome checker web app

---
+++ index.js
@@ -0,0 +1,14 @@
+document.getElementById('Check_btn').addEventListener('click',()=>{
+      var textbox = document.getElementById('textbox').value;
+
+      var text_array  = [textbox];
+     
+
+      let sep_text = textbox.split('');
+       console.log(textbox);
+      var reversed_text = 
+
+      
+})
+
+
