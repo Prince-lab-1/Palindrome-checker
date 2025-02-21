ca967c2348085102e083198b6b5dd103023b3506
Prince-the-Great <bussiness1407@gmail.com>
2025-02-21 17:38:57 +0200

    added some un finished features

--- index.js
+++ index.js
@@ -1,14 +1,43 @@
-document.getElementById('Check_btn').addEventListener('click',()=>{
-      var textbox = document.getElementById('textbox').value;
+var btn = document.querySelector('button');
 
-      var text_array  = [textbox];
-     
 
-      let sep_text = textbox.split('');
-       console.log(textbox);
-      var reversed_text = 
-
-      
+document.addEventListener('keydown',()=>{
+    if (event.key == 'Enter') {
+        check();
+        let result = document.querySelector('input').innerHTML='';
+    }
+    
 })
 
+btn.addEventListener('click',()=>{
+    check();
+});
 
+
+function check() {
+    
+    let txt = document.getElementById('textbox');
+
+    let txt_value  = txt.value;
+
+    let txt_splited = txt_value.split('')
+
+    let txt_reversed = txt_splited.reverse();
+
+    let txt_joined = txt_reversed.join('');
+
+ 
+    
+
+
+    if (txt_value == txt_joined) {
+        
+        let result = document.querySelector('.result');
+
+        result.innerHTML = `${txt_value} is A palindlome !!!`;
+    }else{
+        let result = document.querySelector('.result');
+
+        result.innerHTML = `${txt_value} is not A palindlome !!!`;
+    }
+}
